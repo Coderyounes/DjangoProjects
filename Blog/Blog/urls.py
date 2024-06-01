326a149e0a08a422b9218a3042056343745bec5f
Coderyounes <younesanas2021@gmail.com>
2024-06-01 23:44:11 +0100

    add djsoer & try to Create a new user & login

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user', include("user.urls")),
     path('api/v1/post', include("posts.urls")),
     path('api/v1/comment', include("comments.urls")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/auth/blacklist", TokenBlacklistView.as_view()),
 ]
