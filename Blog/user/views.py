e1e4dcc980fa926df5f2d876c20232acc361e092
Coderyounes <younesanas2021@gmail.com>
2024-06-01 23:18:06 +0100

    add basic get profile view to user views.py

--- Blog/user/views.py
+++ Blog/user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.response import Response
+from .models import Person
 
-# Create your views here.
+
+@api_view(['GET'])
+def profile(request):
+    try:
+        current = Person.objects.get(user=request.user)
+    except ObjectDoesNotExist:
+        return Response({'error': 'no such user'}, status=status.HTTP_404_NOT_FOUND)
+    res = {
+        'username': current.user.username,
+        'firstname': current.user.first_name
+    }
+    return Response(res, status=status.HTTP_200_OK)
+
