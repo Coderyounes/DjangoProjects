45d68a408a55eb441644ce03e5449d873c223972
Coderyounes <younesanas2021@gmail.com>
2024-06-02 19:21:03 +0100

    add some serializers methods in posts

--- Blog/posts/serializers.py
+++ Blog/posts/serializers.py
@@ -10,11 +10,31 @@ class PostsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_author(self, obj):
+        """
+        method to fetch username from user instance
+        return: username or None
+        """
         if obj.user:
             return obj.user.username
         return None
+
     def validate_content(self, value):
+        """
+        Validate 'content' field length
+        Return: raise ValidationError or return value
+        """
         n = len(value)
         if n < 10 or n > 1000:
             raise serializers.ValidationError('Content length must be between 10 and 1000 characters')
         return value
+
+    def validate(self, data):
+        """
+        Validate the existence of fields in data
+        return: Raise ValidationError or return data
+        """
+        required = ['title', 'content']
+        for field in required:
+            if field not in data:
+                raise serializers.ValidationError(f'{field} is required')
+        return data
